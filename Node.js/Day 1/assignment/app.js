f047b569348c560588d07a40a894110c85482c8d
Amit <ambedkaramit176@gmail.com>
2024-02-17 17:09:35 +0530

    adding  day 1 assignment

---
+++ Node.js/Day 1/assignment/app.js
@@ -0,0 +1,7 @@
+const add = 10 + 2;
+// console.log(`The added value is ${add}`);
+console.log("The added value is " + add);
+
+const subs = 10 - 2;
+// console.log(`The Subtracted value is ${subs}`);
+console.log("he subtracted value is " + subs);
